5f0a06866ad75311f9b02688cbcbe52da57669c5
DoNgocPhuc <phucngocdo@gmail.com>
2026-04-10 07:44:35 +0700

    update

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -18,13 +18,21 @@ package org.springframework.samples.petclinic.system;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 class WelcomeController {
 
 	@GetMapping("/")
 	public String welcome() {
+
 		return "welcome";
+	}
+
+	@GetMapping("/aaaa")
+	@ResponseBody
+	public String welcome1() {
+		return "xcxcxcxc";
 	}
 
 }
